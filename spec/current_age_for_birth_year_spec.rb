d3f51816f36f90dc233c85ce1cbf2a79fe03e10f
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-16 21:54:04 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -1,9 +1,17 @@
 require_relative '../current_age_for_birth_year.rb'
 
+def current_age_for_birth_year(birth_year)
+
 describe "current_age_for_birth_year method" do
   it "returns the age of a person based on the year of birth" do
     age_of_person = current_age_for_birth_year(1984)
 
     expect(age_of_person).to eq(19)
-  end
+  endhttps://learn.co/tracks/web-development-fundamentals/intro-to-ruby/introduction/intro-to-tests-rspec-and-learn#
+  
+  def current_age_for_birth_year
+    
+    current_age_for_birth_year(birth_year)
+  2003 - birth_year
+    
 end

d0724841c86d8e3c5336b12603ffdb9bb4867c68
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-04-16 22:05:42 +0000

    Automatically backed up by Learn

--- spec/current_age_for_birth_year_spec.rb
+++ spec/current_age_for_birth_year_spec.rb
@@ -2,8 +2,7 @@ require_relative '../current_age_for_birth_year.rb'
 
 def current_age_for_birth_year(birth_year)
   
-  def current_age_for_birth_year
-end
+  
 
 current_age_for_birth_year.rb
 describe "current_age_for_birth_year method" do
